bd8082166fa4b1c4f688377c7709ee755b55e1fd
zakuk <6840788+zakuk@users.noreply.github.com>
2019-05-07 12:25:52 +0100

    Demo application added that targets the new dot net core version of the library

---
+++ Demo.ApplicationCore/DatabaseContext/UserFluentMap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Numero3.EntityFramework.Demo.DomainModel;
+
+namespace Numero3.EntityFramework.Demo.DatabaseContext
+{
+	/// <summary>
+	/// Defines the convention-based mapping overrides for the User model. 
+	/// </summary>
+	public class UserFluentMap : IEntityTypeConfiguration<User>
+	{
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(m => m.Name).IsRequired();
+            builder.Property(m => m.Email).IsRequired();
+        }
+    }
+}
---
+++ Demo.ApplicationCore/DatabaseContext/UserManagementDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Numero3.EntityFramework.Demo.DomainModel;
+
+namespace Numero3.EntityFramework.Demo.DatabaseContext
+{
+	public class UserManagementDbContext : DbContext
+	{
+		// Map our 'User' model by convention
+		public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=DbContextScopeDemo;Trusted_Connection=true;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			// Overrides for the convention-based mappings.
+			// We're assuming that all our fluent mappings are declared in this assembly.
+			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetAssembly(typeof(UserManagementDbContext)));
+		}
+	}
+}
